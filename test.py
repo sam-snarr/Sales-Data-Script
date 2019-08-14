14271115ac9697678b741241d136b0038a2537f0
Sam Snarr <38705645+sam-snarr@users.noreply.github.com>
2019-08-13 21:30:19 -0400

    added documentation

--- test.py
+++ test.py
@@ -11,16 +11,20 @@ import pandas as pd
 dirpath = os.path.dirname(os.path.realpath(__file__))
 print(listdir(dirpath))
 
+# gets full path of all files in same directory as script if it contains keywords 'apptopia' or 'mighty'
 onlyfiles = [join(dirpath, f) for f in listdir(dirpath) if isfile(join(dirpath, f)) and ('mighty' in f.lower() or 'apptopia' in f.lower())] #and f != os.path.basename(__file__)
 
+# removes python script from onlyfiles using set subtraction
 onlyfiles = set(onlyfiles) - set([os.path.abspath(__file__)])
 
+# initialize arrays for dataframes
 apptopia_df_array = []
 mighty_df_array = []
 
 print('onlyfiles: ', onlyfiles)
 print('os.path.abspath(__file__):', os.path.abspath(__file__))
 
+# adds each file to relevant array and gives it column with what platform it came from
 for file in onlyfiles:
     #print(file)
     f = file.lower()
@@ -41,12 +45,19 @@ for file in onlyfiles:
     elif ('mighty' in f) and ('android' in f):
         temp_df['Platform'] = 'Android'
         mighty_df_array.append(temp_df)
+    else:
+    	print('___At least one relevant file was not used___')
         
 
+# concatenate dataframe arryas
 apptopia_df = pd.concat(apptopia_df_array) 
 mighty_df = pd.concat(mighty_df_array)
 
-final_df = pd.merge(left=apptopia_df, right=mighty_df, left_on=['App Name', 'Platform'], right_on=['App_Name', 'Platform'])
+# merge dataframes on app name and platform
+final_df = pd.merge(left=apptopia_df, right=mighty_df, left_on=['App Name', 'Platform'], right_on=['App_Name', 'Platform']) # may need to update left_on/right_on
 
+# output csv
 final_df.to_csv(r'final df.csv')
+
+# close terminal
 input('Enter to close')
